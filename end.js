a82861b60e56933b4b6e54aa1ed5d7e00926890a
Maksim Yermalovich <m_yermalovich@wargaming.net>
2014-08-05 13:52:35 +0300

    fix bug of closing widget

--- end.js
+++ end.js
@@ -1,7 +1,13 @@
 (function(window) {
-    $('.gridster ul').on('click', function (e) {
+    var $gridsterContainer = $('.gridster ul');
+
+    $gridsterContainer.on('click', function (e) {
         _.each(window.newPage.enabledWidgets, function (widget, widgetName) {
             widget.closeContent(e);
         });
+    });
+
+    $gridsterContainer.find('li').on('click', function (e) {
+      return false;
     });
 })(window);
