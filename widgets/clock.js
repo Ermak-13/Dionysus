a0532d76ed61c68f17c77a5a91b62f63c65e2e58
Maksim Yermalovich <yermalovich.m@gmail.com>
2014-08-09 21:43:49 +0300

    refactoring TimeWidget

--- widgets/clock.js
+++ widgets/clock.js
@@ -1,24 +1,47 @@
 (function(window, page, Views, Models, globalSettings, widgetSettings) {
-    var ClockView = Views.Widget.extend({
+    var TimeModel = Backbone.Model.extend({
+            initialize: function(time) {
+                this.time = time;
+            },
+
+            toJSON: function () {
+                return {
+                    hours: this.getHours(),
+                    minutes: this.getMinutes(),
+                    seconds: this.getSeconds(),
+                    milliseconds: this.getMilliseconds()
+                }
+            },
+
+            getHours: function () {
+                return this.getNiceFormat(this.time.getHours());
+            },
+
+            getMinutes: function () {
+                return this.getNiceFormat(this.time.getMinutes());
+            },
+
+            getSeconds: function () {
+                return this.getNiceFormat(this.time.getSeconds());
+            },
+
+            getMilliseconds: function () {
+                return this.getNiceFormat(this.time.getMilliseconds());
+            },
+
+            getNiceFormat: function (number, limit) {
+                var limit = limit || 10;
+                return (number < limit ? "0" : "") + number;
+            }
+        }),
+
+        ClockView = Views.Widget.extend({
             widgetName: 'clock',
             template: $('#widget-clock-template').html(),
 
             getContext: function() {
-                var getNiceFormat = function(number, limit) {
-                        var limit = limit || 10;
-                        return (number < limit ? "0" : "") + number;
-                    },
-                    currentDateTime = new Date(),
-                    clock = $(
-                        '<span><span>'
-                    );
-
-                return {
-                    'hours': getNiceFormat(currentDateTime.getHours()),
-                    'minutes': getNiceFormat(currentDateTime.getMinutes()),
-                    'seconds': getNiceFormat(currentDateTime.getSeconds()),
-                    'milliseconds': getNiceFormat(currentDateTime.getMilliseconds(), 100)
-                };
+                var time = new TimeModel(new Date());
+                return time.toJSON();
             }
         });
 
